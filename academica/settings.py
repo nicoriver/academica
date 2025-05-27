ef8fce3b355119daf90c8a8cb03a5148e393f97c
nicoriver <fabio.rivero@gmail.com>
2025-05-27 20:46:17 -0300

    cambio en settings en BASE_DIR

--- academica/settings.py
+++ academica/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'academica.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # <- Esto es lo importante,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,7 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-import os
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "gestioncursos.html")
-
 # Create your views here.
-TEMPLATE_DIRS = (
-    'os.path.join(BASE_DIR, "templates")'
-)
+#TEMPLATE_DIRS = (
+#    'os.path.join(BASE_DIR, "templates")'
+#)
 
 def index(request):
     return render(request, "index.html")
+
+def home(request):
+    return render(request, "gestioncursos.html")
