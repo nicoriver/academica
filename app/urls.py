b6a1995821dbd57e25449fe2e54aa6e4c6971de8
nicoriver <fabio.rivero@gmail.com>
2025-05-28 17:02:25 -0300

    cambios en URLS

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from django import views
 from . import views
 urlpatterns = [   
-    path('index/', views.index, name="index"),
+    path('', views.index, name="index"),
 ]   
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-#TEMPLATE_DIRS = (
-#    'os.path.join(BASE_DIR, "templates")'
-#)
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates")'
+)
 
 def index(request):
     return render(request, "index.html")
