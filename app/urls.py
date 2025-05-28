d1462c71c105c0ce198f9d46a2583e44cf5d3a81
nicoriver <fabio.rivero@gmail.com>
2025-05-27 21:23:05 -0300

    cambio en urls.py de APP

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-urlpatterns = [
-    path('', views.home),
+urlpatterns = [   
     path('index/', views.index, name="index"),
 ]   
--- app/views.py
+++ app/views.py
@@ -8,5 +8,3 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
 
-def home(request):
-    return render(request, "gestioncursos.html")
