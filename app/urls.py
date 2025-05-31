ea79668ac1058d155fa94a36a8b6859c6c33ba36
nicoriver <fabio.rivero@gmail.com>
2025-05-30 22:32:50 -0300

    paginas listar y agregar

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,8 @@ from django import views
 from . import views
 urlpatterns = [   
     path('', views.index, name="index"),
+    path('listar', views.list, name="list"),
+    path('delete', views.delete, name="delete"),
+    path('add', views.add, name="add"),
+    path('update', views.update, name="update"),
 ]   
--- app/views.py
+++ app/views.py
@@ -7,4 +7,16 @@ TEMPLATE_DIRS = (
 
 def index(request):
     return render(request, "index.html")
+def update(request):
+    return render(request, "crud_usuarios/update.html")
+
+def list(request):
+    return render(request, "crud_usuarios/list.html")
+
+def delete(request):
+    return render(request, "crud_usuarios/delete.html")
+
+def add(request):
+    return render(request, "crud_usuarios/add.html")
+
 
